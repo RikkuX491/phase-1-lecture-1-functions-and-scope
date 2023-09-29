2ddd1272646687f3075e8e11cc7982a6fb090875
Ricardo Guerra <RikkuG491@gmail.com>
2023-09-29 16:35:36 -0400

    Added solution code

--- src/index.js
+++ src/index.js
@@ -10,3 +10,38 @@ const foodImageLinks = [
     "./assets/food/hot-dog.jpeg",
     "./assets/food/onion-rings.jpeg"
 ]
+
+function sum(num1, num2){
+    return num1 + num2
+}
+
+function getSumString(num1, num2){
+    const sumString = `${num1} + ${num2} = ${sum(num1, num2)}`
+    return sumString
+}
+
+function appendSumStringToPage(num1, num2){
+    const h1 = document.createElement('h1')
+    h1.textContent = getSumString(num1, num2)
+    document.body.append(h1)
+}
+
+function getRandomNumber(){
+    return Math.ceil(Math.random() * 100)
+}
+
+// for(let counter = 1; counter <= 100; counter++){
+//     appendSumStringToPage(getRandomNumber(), getRandomNumber())
+// }
+
+function addFoodImageToRestaurantMenu(foodImageLink){
+    const img = document.createElement('img')
+    img.src = foodImageLink
+    restaurantMenu.append(img)
+}
+
+const restaurantMenu = document.getElementById('restaurant-menu')
+
+for(let index = 0; index < foodImageLinks.length; index++){
+    addFoodImageToRestaurantMenu(foodImageLinks[index])
+}
