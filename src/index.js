fcb47b4880654ab9198ef92396733615a12c899b
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-02 16:03:14 -0400

    Added solution code for lecture

--- src/index.js
+++ src/index.js
@@ -10,3 +10,33 @@ const foodImageLinks = [
     "./assets/food/hot-dog.jpeg",
     "./assets/food/onion-rings.jpeg"
 ]
+
+function sum(num1, num2){
+    return num1 + num2
+}
+
+function getSumString(num1, num2){
+    return `${num1} + ${num2} = ${sum(num1, num2)}`
+}
+
+function addSumStringToPage(num1, num2){
+    let h1 = document.createElement('h1')
+    h1.textContent = getSumString(num1, num2)
+    document.body.append(h1)
+}
+
+function getRandomNumber(){
+    return Math.ceil(Math.random() * 100)
+}
+
+// for(let counter = 1; counter <= 100; counter++){
+//     addSumStringToPage(getRandomNumber(), getRandomNumber())
+// }
+
+const restaurantMenu = document.getElementById('restaurant-menu')
+
+for(let index = 0; index < foodImageLinks.length; index++){
+    const imgElement = document.createElement('img')
+    imgElement.src = foodImageLinks[index]
+    restaurantMenu.appendChild(imgElement)
+}
